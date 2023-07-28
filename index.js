9122b3f9cb3b8ba988b11e0db646b3b286b27fc8
Davide Marchica <123973018+davide122@users.noreply.github.com>
2023-07-29 01:11:03 +0200

    Add files via upload
    4

--- index.js
+++ index.js
@@ -27,3 +27,43 @@ balloons.forEach((balloon) => {
 });
 
 
+
+const audio = document.getElementById('custom-audio');
+const playBtn = document.querySelector('.play-btn');
+const volumeBtn = document.querySelector('.volume-btn');
+const progressBar = document.querySelector('.progress');
+const timeDisplay = document.querySelector('.time-display');
+
+let isPlaying = false;
+
+playBtn.addEventListener('click', togglePlay);
+volumeBtn.addEventListener('click', toggleMute);
+audio.addEventListener('timeupdate', updateProgress);
+audio.addEventListener('ended', () => (isPlaying = false));
+
+function togglePlay() {
+  if (isPlaying) {
+    audio.pause();
+  } else {
+    audio.play();
+  }
+  isPlaying = !isPlaying;
+  updatePlayButton();
+}
+
+function toggleMute() {
+  audio.muted = !audio.muted;
+  volumeBtn.classList.toggle('muted', audio.muted);
+}
+
+function updatePlayButton() {
+  playBtn.classList.toggle('playing', isPlaying);
+}
+
+function updateProgress() {
+  const progress = (audio.currentTime / audio.duration) * 100;
+  progressBar.style.width = `${progress}%`;
+  const minutes = Math.floor(audio.currentTime / 60);
+  const seconds = Math.floor(audio.currentTime % 60);
+  timeDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
